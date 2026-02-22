0f1e5276c748db94dfb8b222b5f1c72338acdd62
Dhruti-007 <pp2909@srmist.edu.in>
2026-02-23 02:55:03 +0530

    Added UC3 file

---
+++ UC3.java
@@ -0,0 +1,24 @@
+
+//OOPSBannerApp UC3 
+public class OOPSBannerAppUC3 {
+    
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println(String.join("  ***   " + " " + "  ***   " + " " + " *****  " + " " + " *****  "));
+        
+        System.out.println(String.join(" *   *  " + " " + " *   *  " + " " + " *    * " + " " + "*      "));
+        
+        System.out.println(String.join("*     * " + " " + "*     * " + " " + " *    * " + " " + "*      "));
+        
+        System.out.println(String.join("*     * " + " " + "*     * " + " " + " *****  " + " " + " *****  "));
+        
+        System.out.println(String.join("*     * " + " " + "*     * " + " " + " *      " + " " + "      * "));
+        
+        System.out.println(String.join(" *   *  " + " " + " *   *  " + " " + " *      " + " " + "*      *"));
+        
+        System.out.println(String.join("  ***   " + " " + "  ***   " + " " + " *      " + " " + " *****  "));
+        
+    }
+}
